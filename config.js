97c4c3c811c4242c61ec8e8682e1dcb33ba4bd32
Adam Golab <adam@brainhub.pl>
2018-02-25 12:48:11 +0100

    feat: configured application to work with https certs

--- config.js
+++ config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  PORT: process.env.PORT || 8080,
-};
+const path = require('path');
+
+const fileReader = require('./services/fileReader');
+
+module.exports = Promise.all([
+  fileReader(path.join(__dirname, './certs/cert.pem')),
+  fileReader(path.join(__dirname, './certs/key.pem')),
+])
+  .then(([cert, key]) => ({
+    PORT: process.env.PORT || 8080,
+    WEBSOCKET_PORT: process.env.WEBSOCEKT_PORT || 8090,
+    KEY: key,
+    CERT: cert,
+  }));
+
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require('express');
+const https = require('https');
 
+const app = require('./app');
 const config = require('./config');
 const logger = require('./services/logger');
 
-const app = express();
+config
+  .then(config => {
+    const listen = https.createServer({
+      key: config.KEY,
+      cert: config.CERT,
+      requestCert: false,
+      rejectUnauthorized: false,
+    }, app).listen(config.PORT);
+    logger.info(`Application started on port: ${listen.address().port}`);
 
-const listen = app.listen(config.PORT);
-logger.info(`Application started on port: ${listen.address().port}`);
+    process.on('SIGTERM', () => {
+      listen.close();
+    });
 
-process.on('SIGTERM', () => {
-  listen.close();
-});
-
-process.on('SIGINT', () => {
-  listen.close();
-});
-
+    process.on('SIGINT', () => {
+      listen.close();
+    });
+  })
+  .catch(error => {
+    logger.error(error);
+    process.exit(1)
+  });
