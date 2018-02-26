7254fe5cc0ef3b8e54544e5254187ce295182baf
Adam Golab <adam@brainhub.pl>
2018-02-26 20:18:55 +0100

    feat: correctly handle register action

--- store/sideEffects.js
+++ store/sideEffects.js
@@ -3,5 +3,9 @@ const {
 } = require('./actionTypes');
 
 module.exports = {
-  [REGISTER]: ({ connection }) => connection.send(JSON.stringify({ errors: 0 })),
+  [REGISTER]: ({ connection, payload }) => connection.send(JSON.stringify({
+    error: 0,
+    deviceid: payload.deviceid,
+    apikey: '123456789-1234-1234-1234-123456789123',
+  })),
 };
