13a9dc86814377f95cf9aa2c3e42c40122a265e5
Adam Golab <adam@brainhub.pl>
2018-02-26 21:22:36 +0100

    feat: added routes for getting information about devices

--- routes/devicesRouter.js
+++ routes/devicesRouter.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const httpStatus = require('http-status-codes');
+
+const store = require('../store');
 
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
-  res.send('LIST OF ALL DEVICES');
+  const state = store.getState();
+  const devicesId = Object.keys(state);
+
+  const devices = devicesId.map(id => ({
+    id,
+    state: state[id].params.switch,
+  }));
+
+  res.send(devices);
 });
 
 router.get('/:deviceId', (req, res) => {
-  res.send(`STATUS OF DEVICE WITH ID: ${req.params.deviceId}`);
+  const id = req.params.deviceId;
+  const state = store.getState();
+
+  if (!state[id]) {
+    return res.status(httpStatus.NOT_FOUND).send(httpStatus.getStatusText(httpStatus.NOT_FOUND));
+  }
+
+  const device = {
+    id,
+    state: state[id].params.switch,
+  };
+
+  res.send(device);
 });
 
 router.get('/:deviceId/on', (req, res) => {
