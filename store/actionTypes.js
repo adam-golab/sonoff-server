a56f21f67c68b802c4e012147343c5c5255c64a3
Adam Golab <adam@brainhub.pl>
2018-02-26 20:35:00 +0100

    feat: handle date action

--- store/actionTypes.js
+++ store/actionTypes.js
@@ -1,4 +1,5 @@
 module.exports = {
   DISCONNECT: 'DISCONNECT',
   REGISTER: 'register',
+  DATE: 'date',
 };
--- store/sideEffects.js
+++ store/sideEffects.js
@@ -1,11 +1,19 @@
+const send = require('../services/wsSender');
 const {
   REGISTER,
+  DATE,
 } = require('./actionTypes');
 
 module.exports = {
-  [REGISTER]: ({ connection, payload }) => connection.send(JSON.stringify({
+  [REGISTER]: ({ connection, payload }) => send(connection, {
     error: 0,
     deviceid: payload.deviceid,
     apikey: '123456789-1234-1234-1234-123456789123',
-  })),
+  }),
+  [DATE]: ({ connection, payload }) => send(connection, {
+    error: 0,
+    date: new Date().toISOString(),
+    deviceid: payload.deviceid,
+    apikey: '123456789-1234-1234-1234-123456789123',
+  }),
 };
