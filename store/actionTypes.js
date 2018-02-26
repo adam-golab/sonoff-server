ef0b50b73c25211f018d652d7fb5c321e60bf66b
Adam Golab <adam@brainhub.pl>
2018-02-26 19:59:32 +0100

    feat: create reducer for REGISTER action

---
+++ store/actionTypes.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DISCONNECT: 'DISCONNECT',
+  REGISTER: 'register',
+};
--- store/reducer.js
+++ store/reducer.js
@@ -1,7 +1,20 @@
+const logger = require('../services/logger');
 const initialState = require('./initialState');
+const {
+  REGISTER,
+} = require('./actionTypes');
 
 module.exports = (state = initialState, action) => {
+  logger.log(action.type, action.id);
   switch (action.type) {
+    case REGISTER:
+      return Object.assign({}, {
+        [action.id]: {
+          deviceInfo: action.payload,
+          connection: action.connection,
+          enabled: false,
+        },
+      }, state);
     default:
       return state;
   }
