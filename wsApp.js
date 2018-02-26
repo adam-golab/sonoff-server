eec36bc9135b5bf2a91ae105ac28484ec6b0b863
Adam Golab <adam@brainhub.pl>
2018-02-26 19:58:32 +0100

    feat: connect websockets with the redux

--- wsApp.js
+++ wsApp.js
@@ -1,12 +1,22 @@
-const logger = require('./services/logger');
 const uuid = require('uuid/v4');
+
+const logger = require('./services/logger');
+const store = require('./store');
+const { DISCONNECT } = require('./store/actionTypes');
 
 module.exports = connection => {
   const id = uuid();
 
-  connection.on('text', text => logger.log('TEXT', id, text));
+  connection.on('text', text => {
+    const payload = JSON.parse(text);
+    const action = { id, type: payload.action, payload, connection };
+    store.dispatch(action);
+  });
 
-  connection.on('close', () => logger.log('CLOSE', id));
+  connection.on('close', () => {
+    const action = { type: DISCONNECT, id };
+    store.dispatch(action);
+  });
 
   connection.on('error', logger.error);
 };
